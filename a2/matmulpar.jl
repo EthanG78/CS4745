3dd74b07155faa98c81af023255ed23e2c61920c
EthanG78 <ethangarnier78@gmail.com>
2023-02-01 13:12:24 -0400

    started writing test functions for matmulpar. added function signatures

---
+++ a2/matmulpar.jl
@@ -0,0 +1,12 @@
+#=
+    matmulpar.jl
+    Author: Ethan Garnier
+    Class: CS4745
+    Assignment: 2
+=#
+function matmulpar1!(c, a, b)
+    c = a * b
+end
+
+function matmulpar2!(c, a, b, cutoff)
+end
---
+++ a2/matmulpar_test.jl
@@ -0,0 +1,35 @@
+#=
+    matmulpar_test.jl
+    Author: Ethan Garnier
+    Class: CS4745
+    Assignment: 2
+=#
+using Random
+function matmulpar_test(reps=1, matSize, method=matmulpar1!)
+    for i in 1:reps
+        # Generate random sizes of random arrays
+        m = rand(1:matSize)
+        n = rand(1:matSize)
+        p = rand(1:matSize)
+
+        # Array a is mxn
+        a = rand(Float64, (m, n))
+
+        # Array b is nxp
+        b = rand(Float64, (n, p))
+
+        # Array c is mxp
+        c = zeros(Float64, m, p)
+
+        # Temporary, just testing
+        method(c, a, b)
+        
+        # Compute matrix product using Julia's implementation
+        cc = a * b
+
+        # Compare results
+        diff = norm((cc-c)./c)
+
+        print("Diff: $diff")
+    end
+end
