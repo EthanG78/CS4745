cc4e4a10fcbaafc903a638dba8beb530593bc5f2
EthanG78 <ethangarnier78@gmail.com>
2023-02-04 19:28:24 -0400

    using @views macro in assgn 2 as to not allocate more memeory. Working on dividing arrays

--- a2/matmulpar.jl
+++ a2/matmulpar.jl
@@ -18,19 +18,26 @@ function matmulpar1!(c, a, b)
     nothing
 end
 
+# TODO: BROKEN
 # Fork-join matrix multiplication
 #   - Each matrix is an nxn matrix
 #   - Assume n is a power of two
 function matmulpar2!(c, a, b, cutoff)
-    if length(a) ==1 && length(b) == 1
+    if length(a) == 1 && length(b) == 1
         # Base case
-        c = a * b
+        @views c[1, 1] += a[1 , 1] * b[1, 1]
     else
         # Divide a and b into 4 sub arrays of size (n/2)x(n/2)
-        n = size(a, 1)
-        for i in 1:4
-            a_new = @view a[1:2, 1:2]
-            b_new = @view b[1:2, 1:2]
+        n::Int = size(a, 1) / 2
+        for i in 1:2
+            for j in 1:2
+                # NOT WORKING
+                a_new = @view a[(i*1):(i*n), (j*1):(j*n)]
+                b_new = @view b[(i*1):(i*n), (j*1):(j*n)]
+
+                print("$a_new\n")
+                print("$b_new\n")
+            end
         end
     end
     nothing
