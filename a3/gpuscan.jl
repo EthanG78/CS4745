91777f45dea307c2531984c28365d83c4e49bff0
EthanG78 <ethangarnier78@gmail.com>
2023-02-19 12:59:27 -0400

    Finished CUArray implementation of Hillis and Steele Scane

--- a3/gpuscan.jl
+++ a3/gpuscan.jl
@@ -7,7 +7,13 @@
 using CUDA
 
 function gpuscan_cuarray(a_d)
-    nothing
+    n = length(a_d)
+    j = 1
+    @views @inbounds while j < n
+        a_d[1+j:n] .= a_d[1+j:n] + a_d[1:n - j]
+        j = j << 1
+    end
+    return a_d
 end
 
 function gpuscan_kernel(a_d)
