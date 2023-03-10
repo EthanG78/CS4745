b867ffcd7690f8a28c93cdf716bd4f9953866008
EthanG78 <ethangarnier78@gmail.com>
2023-03-10 18:37:10 -0400

    partition! function is properly parallelized, however at some random edge cases it crashes out of bounds. need testing

--- a4/kmedianpar.jl
+++ a4/kmedianpar.jl
@@ -7,7 +7,9 @@
 using Base.Threads
 
 function kmedianpar(ain, k)
-    if k > lastindex(ain) return -1 end
+    if k > lastindex(ain)
+        return -1
+    end
     a = copy(ain)
     acopy = similar(a)
     return kmedianrec!(a, acopy, 1, length(a), k)
@@ -16,51 +18,54 @@ end
 function kmedianrec!(a, acopy, lo, hi, k)
     ipivot = rand(lo:hi)
     p = a[ipivot]
-    sizeL, idxH = partition!(a, acopy, lo, hi, ipivot)
+    sizeL = partition!(a, acopy, lo, hi, ipivot)
     if sizeL == k - 1 return p end
-    if sizeL >= k return kmedianrec!(acopy, a, lo , lo + sizeL - 1, k) end
-    return kmedianrec!(acopy, a, idxH, hi, k - sizeL - 1)
+    if sizeL >= k return kmedianrec!(acopy, a, lo, lo + sizeL - 1, k) end
+    return kmedianrec!(acopy, a, lo + sizeL, hi - 1, k - sizeL - 1)
 end
 
-# TODO: THIS HAS NOT BEEN TESTED
+# BROKEN
 function partition!(a, acopy, lo, hi, ipivot)
     a[lo], a[ipivot] = a[ipivot], a[lo]
 
     p = a[lo]
 
-    if lo + 1 > hi return hi - lo, hi end
+    if lo + 1 > hi return 1 end
 
     # Populate the marker arrays
-    markerL = similar(lo:hi-1)
-    markerH = similar(lo:hi-1)
-    @threads for i in lo:hi-1
+    markerL = similar(lo+1:hi)
+    markerH = similar(lo+1:hi)
+    @threads for i in lo+1:hi
         if a[i] < p
-            markerL[i] = 1
-            markerH[i] = 0
+            markerL[i-1] = 1
+            markerH[i-1] = 0
         else
-            markerL[i] = 0
-            markerH[i] = 1
+            markerL[i-1] = 0
+            markerH[i-1] = 1
         end
     end
 
+    # Calculate the prefix sum of the marker arrays
+    # to determine where to place values in acopy
     pSumL = cumsum(markerL)
     pSumH = cumsum(markerH)
 
-    sizeL = findmax(pSumL)
+    # The maximum value in the prefix sum of the L marker
+    # array tells us how big the L subset is.
+    sizeL = maximum(pSumL)
 
     # Use the prefix sums to properly place values
     # from a into acopy based on if they are in L or H
-    @threads for i in lo:hi-1
-        if markerL[i] == 1
-            acopy[pSumL[i]] = a[i]
+    @threads for i in lo+1:hi
+        if markerL[i-1] == 1
+            acopy[pSumL[i-1]] = a[i]
         else
-            acopy[pSumH[sizeL + i]] = a[i]
+            acopy[sizeL+pSumH[i-1]] = a[i]
         end
     end
 
+    # debug
+    # @show a, acopy, markerL, markerH, pSumH, pSumL, sizeL
 
-
-    # TODO:
-    #return j - lo, j + 1
-    return 1, 1
+    return sizeL
 end
